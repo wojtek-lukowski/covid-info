4e52d4b8c7bf3e4276f3acc4f5de07eb133effb8
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-02-15 18:23:15 +0100

    array date & daily cases

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
 const countriesApi = 'https://api.covid19api.com/countries';
 
 const country = 'germany';
+// const country = 'poland';
+
 const api = `https://api.covid19api.com/total/country/${country}`;
 
-
-//getting all the countries
-(async function getCountries () {
-  const data = await (await fetch(countriesApi)).json();
-  // console.log(data);
-  const countriesList = data.map(({Country}) => Country);
-  // console.log(countriesList);
-})();
+const daysBack = 15;
 
 (async function getSummary () {
   const data = await (await fetch(api)).json();
-  const last2WeeksCummulatives = data.slice(-15).map(({Confirmed}) => Confirmed);
+  const last2WeeksCummulatives = data.slice(-daysBack).map(({Confirmed}) => Confirmed);
 
   const dailyCases = [];
+  const today = new Date()
+  let startingDate = new Date(today);
+  startingDate.setDate(startingDate.getDate() - daysBack + 1);
+
   for (let i = 0; i < last2WeeksCummulatives.length - 1; i++ ) {
-    dailyCases.push(last2WeeksCummulatives[i+1] - last2WeeksCummulatives[i])
+  let newCases = last2WeeksCummulatives[i+1] - last2WeeksCummulatives[i];
+
+    let dayBefore = new Date(startingDate);
+    dayBefore.setDate(dayBefore.getDate() + i);
+   let day = dayBefore.toLocaleString();
+
+   let object = {day, newCases}
+
+  dailyCases.push(object);
+
   }
   console.log(country, 'daily cases', dailyCases);
 
-  // const last2WeeksDates= data.slice(-14).map(({Date}) => Date);
-  // console.log(last2WeeksDates);
-
-  // document.querySelector('.countries').innerHTML = JSON.stringify(data.slice(-14));
-  document.querySelector('.countries').innerHTML = dailyCases;
+  document.querySelector('.country').innerHTML = country;
+  document.querySelector('.countries').innerHTML = JSON.stringify(dailyCases);
 })();
 
-// "/dayone/country/:country/status/:status/live
+getCountry = () => {
+  let countryInput = document.getElementById('country').value;
+  console.log('new country', countryInput);
+  return countryInput;
+}
+
+//getting all the countries
+// (async function getCountries () {
+//   const data = await (await fetch(countriesApi)).json();
+//   // console.log(data);
+//   const countriesList = data.map(({Country}) => Country);
+//   // console.log(countriesList);
+// })();
