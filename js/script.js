c7d87d755ba0e89894c38635d3a324840bcb27ec
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-02-16 12:01:39 +0100

    Cleaning before connecting charts

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 const countriesApi = 'https://api.covid19api.com/countries';
 
 const country = 'germany';
-// const country = 'poland';
 
 const api = `https://api.covid19api.com/total/country/${country}`;
+// const api = `https://api.covid19api.com/premium/country/south-africa`;
 
 //number of days will be lower by 1
-const daysBack = 15;
+const daysBack = 31;
 
 (async function getSummary () {
+  document.querySelector('.loading').style.display = "block";
   const data = await (await fetch(api)).json();
+  console.log('data', data);
+  // console.log('premium', data);
   const cummulatedPeriod = data.slice(-daysBack).map(({Confirmed}) => Confirmed);
+  document.querySelector('.loading').style.display = "none";
 
 
   // creating an array with object where an object contain date and daily new cases. The daily new cases
@@ -31,7 +35,7 @@ const daysBack = 15;
   }
 
   document.querySelector('.country').innerHTML = country;
-  document.querySelector('.countries').innerHTML = JSON.stringify(dailyCases);
+  document.querySelector('.country-data').innerHTML = JSON.stringify(dailyCases);
 
   const casesArray = dailyCases.map(({newCases}) => newCases);
   document.querySelector('.today').innerHTML = JSON.stringify(casesArray[casesArray.length - 1]);
@@ -43,10 +47,29 @@ getCountry = () => {
   return countryInput;
 }
 
-//getting all the countries
-// (async function getCountries () {
-//   const data = await (await fetch(countriesApi)).json();
-//   // console.log(data);
-//   const countriesList = data.map(({Country}) => Country);
-//   // console.log(countriesList);
-// })();
+// getting all the countries
+(async function getCountries () {
+  const data = await (await fetch(countriesApi)).json();
+  const countriesList = data.map(({Country}) => Country);
+  const countriesSlugs = data.map(({Slug}) => Slug);
+  const countriesAndSlugs = data.map(({Country, Slug }) => ({Country, Slug}));
+  document.querySelector('.countries-list').innerHTML = JSON.stringify(countriesList);
+  // console.log(countriesAndSlugs);
+  // console.log(countriesList);
+  // console.log(countriesSlugs);
+
+  // const countriesToGet = countriesSlugs.splice(-5);
+  const countriesToGet = ['germany', 'poland', 'united-kingdom', 'italy', 'spain', 'france', 'sweden'];
+  // console.log('shortened list', countriesToGet);
+
+  // const countriesData = countriesList.map((country => await fetch(`https://api.covid19api.com/total/country/${country}`).json()));
+  // const countriesData = countriesToGet.map((country => getCountryData(country)));
+
+  // async function getCountryData (country) {
+  //  const countryLast = await (await fetch('https://api.covid19api.com/total/country/' + country)).json();
+  //   console.log(countryLast);
+  // }
+    
+  // const countriesData = countriesList.map((country => console.log(country)));
+  // console.log(countriesData);
+})();
